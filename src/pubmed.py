f94eefc0fe1d869753ec7bbe5e315c5df6cc8303
alamurias <alamurias@lasige.di.fc.ul.pt>
2015-09-23 15:31:56 +0100

    Return Pubmed title and abstract

--- src/pubmed.py
+++ src/pubmed.py
@@ -12,9 +12,9 @@ def get_pubmed_abs(pmid):
     r1 = conn.getresponse()
     #print "Request Status: " + str(r1.status) + " " + str(r1.reason)
     response = r1.read()
-    #print response[3]
-    abstract = parse_pubmed_xml(response)
-    return abstract, str(r1.status) + ' ' + str(r1.reason)
+    # print response
+    title, abstract = parse_pubmed_xml(response)
+    return title, abstract, str(r1.status) + ' ' + str(r1.reason)
 
     
 def parse_pubmed_xml(xml):
@@ -24,13 +24,14 @@ def parse_pubmed_xml(xml):
         sys.exit()
     else:
         root = ET.fromstring(xml)
+        title = root.findall('.//ArticleTitle').text
         abstext = root.findall('.//AbstractText')
         if len(abstext) > 0:
             abstext = abstext[0].text
         else:
             print "Abstract not found"
             sys.exit()
-    return abstext
+    return title, abstext
     
     
     
