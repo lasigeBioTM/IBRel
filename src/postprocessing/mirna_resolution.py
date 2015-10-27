9d7763023d95e3b1d2e13d903093d9344df12193
AndreLamurias <andrelamurias@gmail.com>
2015-10-27 13:54:32 +0000

    Map miRNA to miRBase

--- src/postprocessing/mirna_resolution.py
+++ src/postprocessing/mirna_resolution.py
@@ -1,3 +1,4 @@
+import atexit
 import sys
 import sqlite3
 import re
@@ -23,18 +24,28 @@ def load_from_file(path):
                 t = (mirna["acc"], mirna["name"], mirna["description"])
                 c.execute("INSERT INTO mirna VALUES (?,?,?)", t)
                 mirna = {}
-    conn.commit()
-    conn.close()
+
 
 def find_mirna(term, organism="human"):
     # add hsa if organism is human
     if not term.startswith("hsa-"):
         term = "hsa-" + term
-    # if
+    c.execute("SELECT acc, name FROM mirna WHERE name=?", (term.lower(),))
+    return c.fetchone()
+
+def exit_handler():
+    conn.commit()
+    conn.close()
+
+atexit.register(exit_handler)
+
 
 def main():
     if sys.argv[1] == "generate": # generate SQLite database for miRNAs
         load_from_file(sys.argv[2]) # path to miRNA tracker file should be the second argument
+    else:
+        res = find_mirna(sys.argv[1])
+        print res
 
 if __name__ == "__main__":
     main()
