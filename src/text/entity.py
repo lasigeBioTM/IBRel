a71b06994965012de5fcb41ba074b1cb04a0f68d
AndreLamurias <andrelamurias@gmail.com>
2015-11-03 14:23:49 +0000

    Move ProteinEntity to new file

--- src/text/entity.py
+++ src/text/entity.py
@@ -69,22 +69,6 @@ class Entity(object):
         return dic
 
 
-class ProteinEntity(Entity):
-    def __init__(self, tokens, **kwargs):
-        # Entity.__init__(self, kwargs)
-        super(ProteinEntity, self).__init__(tokens, **kwargs)
-        self.type = "protein"
-        self.subtype = kwargs.get("subtype")
-
-
-    def get_dic(self):
-        dic = super(ProteinEntity, self).get_dic()
-        dic["subtype"] = self.subtype
-        dic["ssm_score"] = self.ssm_score
-        dic["ssm_entity"] = self.ssm_go_ID
-        return dic
-
-
 class Entities(object):
     """Group of entities related to a text"""
 
