4245260756bdcc3d3672dd90cb5602f694fe33bc
AndreLamurias <andrelamurias@gmail.com>
2015-11-11 14:36:34 +0000

    Protein recognition improvements

--- src/reader/transmir.py
+++ src/reader/transmir.py
@@ -1,4 +1,4 @@
-# read transmir database
+# read transmir database and generate corpus
 
 db_name = "data/transmir_v1.2.tsv"
 tfs = set()
@@ -10,14 +10,14 @@ entries = {} # (tfname, mirname): active
 with open(db_name, 'r') as dbfile:
     for line in dbfile:
         tsv = line.strip().split("\t")
-        if tsv[-1] == "human":
+        if tsv[-1].lower() == "human":
             tfname = tsv[0]
             mirname = tsv[3]
             func = tsv[5].split(";")
             disease = tsv[6].split(";")
             active = tsv[7]
             pmid = tsv[8].split(";")
-            tfs.add(tfname)
+            tfs.add(tfname.replace("-", "")) # uniform TF names
             for f in func:
                 funcs.add(f.strip())
             for d in disease:
@@ -27,16 +27,16 @@ with open(db_name, 'r') as dbfile:
                 pmids.add(p.strip())
             mirnas[mirname] = (func, [d for d in disease if d != "see HMDD (http://cmbi.bjmu.edu.cn/hmdd)"])
             entries[(tfname, mirname)] = (active, pmid)
-print "TF:"
-print tfs
-print "Diseases:"
-print diseases
-print "Functions:"
-print funcs
-print "PMIDs:"
-print pmids
-print "miRNAs:"
-print mirnas.keys()
+# print "TF:"
+# print tfs
+# print "Diseases:"
+# print diseases
+# print "Functions:"
+# print funcs
+# print "PMIDs:"
+# print pmids
+# print "miRNAs:"
+# print mirnas.keys()
 # print "Entries:"
 # print entries
 print "Number of TFs: {}".format(len(tfs))
@@ -45,3 +45,11 @@ print "Number of Functions: {}".format(len(funcs))
 print "Number of PMIDs: {}".format(len(pmids))
 print "Number of miRNAs: {}".format(len(mirnas))
 print "Number of Entries: {}".format(len(entries))
+with open("corpora/transmir/transmir_pmids.txt", 'w') as f:
+    f.write('\n'.join(pmids))
+with open("corpora/transmir/transmir_tfs.txt", 'w') as f:
+    f.write('\n'.join(tfs))
+with open("corpora/transmir/transmir_mirnas.txt", 'w') as f:
+    f.write('\n'.join(mirnas))
+with open("corpora/transmir/transmir_diseases.txt", 'w') as f:
+    f.write('\n'.join(diseases))
--- src/text/protein_entity.py
+++ src/text/protein_entity.py
@@ -1,11 +1,13 @@
 import logging
-
+import re
 from config import config
 from text.entity import Entity
 
 __author__ = 'Andre'
 prot_words = set()
-prot_stopwords = set(["chromosome", "factor", "conserved"])
+prot_stopwords = set(["chromosome", "factor", "conserved", "gene", "anti", "mir", "regulatory", "terminal", "element",
+                      "activator", "cell", "box", "transcriptional", "transcription", "growth", "talk", "epithelial",
+                      "alpha", "microrna", "chip", "chipseq", "interferons", "tweak", "allele"])
 # words that may seem like they are not part of named chemical entities but they are
 
 # words that are never part of chemical entities
@@ -24,6 +26,7 @@ class ProteinEntity(Entity):
         self.subtype = kwargs.get("subtype")
         # print self.sid
 
+    tf_regex = re.compile(r"\A[A-Z]+\d*\w*\d*\Z")
 
     def get_dic(self):
         dic = super(ProteinEntity, self).get_dic()
@@ -39,9 +42,6 @@ class ProteinEntity(Entity):
         :return: True if entity does not fall into any of the rules, False if it does
         """
         if "stopwords" in rules:
-            if self.text.lower().startswith("mir-") or self.text.lower().startswith("microrna"):
-                logging.debug("mirna %s" % self.text)
-                return False
             words = self.text.split(" ")
             words += self.text.split("-")
             stop = False
@@ -51,7 +51,15 @@ class ProteinEntity(Entity):
                     stop = True
             if stop:
                 return False
-        if not self.text[0].isalpha():
+        if "alpha" in rules and not self.text[0].isalpha():
             logging.debug("not alpha %s" % self.text)
             return False
+        if "nwords" in rules:
+            words = self.text.split(" ")
+            if len(words) > 1:
+                return False
+        if "codeonly" in rules:
+            if self.tf_regex.match(self.text) is None:
+                return False
+        self.text = self.text.replace("-", "")
         return True
