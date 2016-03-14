50dfdad606675a69b50d10423f73aee3a26da18a
AndreLamurias <andrelamurias@gmail.com>
2016-03-14 15:31:15 +0000

    SeeDev corpus, first approach

---
+++ src/reader/seedev_corpus.py
@@ -0,0 +1,107 @@
+import logging
+import xml.etree.ElementTree as ET
+import os
+import sys
+
+import itertools
+import progressbar as pb
+import time
+
+from text.corpus import Corpus
+from text.document import Document
+from text.sentence import Sentence
+
+
+
+class SeeDevCorpus(Corpus):
+    """
+    Corpus for the BioNLP SeeDev task
+    self.path is the base directory of the files of this corpus.
+    """
+    def __init__(self, corpusdir, **kwargs):
+        super(SeeDevCorpus, self).__init__(corpusdir, **kwargs)
+        self.subtypes = []
+
+    def load_corpus(self, corenlpserver, process=True):
+        # self.path is the base directory of the files of this corpus
+        trainfiles = [self.path + '/' + f for f in os.listdir(self.path) if f.endswith('.txt')]
+        total = len(trainfiles)
+        widgets = [pb.Percentage(), ' ', pb.Bar(), ' ', pb.AdaptiveETA(), ' ', pb.Timer()]
+        pbar = pb.ProgressBar(widgets=widgets, maxval=total, redirect_stdout=True).start()
+        time_per_abs = []
+        for current, f in enumerate(trainfiles):
+            #logging.debug('%s:%s/%s', f, current + 1, total)
+            print '{}:{}/{}'.format(f, current + 1, total)
+            did = f.split(".")[0]
+            t = time.time()
+            with open(f, 'r') as txt:
+                doctext = txt.read()
+            newdoc = Document(doctext, process=False, did=did)
+            newdoc.sentence_tokenize("biomedical")
+            if process:
+                newdoc.process_document(corenlpserver, "biomedical")
+            self.documents[newdoc.did] = newdoc
+            abs_time = time.time() - t
+            time_per_abs.append(abs_time)
+            #logging.info("%s sentences, %ss processing time" % (len(newdoc.sentences), abs_time))
+            pbar.update(current+1)
+        pbar.finish()
+        abs_avg = sum(time_per_abs)*1.0/len(time_per_abs)
+        logging.info("average time per abstract: %ss" % abs_avg)
+
+    def load_annotations(self, ann_dir, etype, pairtype="all"):
+        annfiles = [ann_dir + '/' + f for f in os.listdir(ann_dir) if f.endswith('.a1')]
+        total = len(annfiles)
+        time_per_abs = []
+        for current, f in enumerate(annfiles):
+            logging.debug('%s:%s/%s', f, current + 1, total)
+            did = f.split(".")[0]
+            with open(f, 'r') as txt:
+                for line in txt:
+                    # print line
+                    tid, ann, etext = line.strip().split("\t")
+                    entity_type, dstart, dend = ann.split(" ")
+                    # load all entities
+                    #if etype == "all" or (etype != "all" and etype == type_match[entity_type]):
+                    dstart, dend = int(dstart), int(dend)
+                    sentence = self.documents[did].find_sentence_containing(dstart, dend, chemdner=False)
+                    if sentence is not None:
+                        # e[0] and e[1] are relative to the document, so subtract sentence offset
+                        start = dstart - sentence.offset
+                        end = dend - sentence.offset
+                        sentence.tag_entity(start, end, entity_type, text=etext, original_id=tid)
+                    else:
+                        print "could not find sentence for this span: {}-{}".format(dstart, dend)
+
+        # self.evaluate_normalization()
+
+def get_seedev_gold_ann_set(goldpath, entitytype, pairtype):
+    logging.info("loading gold standard annotations... {}".format(goldpath))
+    annfiles = [goldpath + '/' + f for f in os.listdir(goldpath) if f.endswith('.a1')]
+    gold_offsets = set()
+    tid_to_offsets = {}
+    for current, f in enumerate(annfiles):
+            did = f.split(".")[0]
+            with open(f, 'r') as txt:
+                for line in txt:
+                    tid, ann, etext = line.strip().split("\t")
+                    etype, dstart, dend = ann.split(" ")
+                    dstart, dend = int(dstart), int(dend)
+                    tid_to_offsets[tid] = (did, dstart, dend, etext)
+    gold_relations = set()
+    annfiles = [goldpath + '/' + f for f in os.listdir(goldpath) if f.endswith('.a2')]
+    for current, f in enumerate(annfiles):
+            did = f.split(".")[0]
+            with open(f, 'r') as txt:
+                for line in txt:
+                    eid, ann = line.strip().split("\t")
+                    etype, sourceid, targetid = ann.split(" ")
+                    sourceid = sourceid.split(":")[-1]
+                    targetid = targetid.split(":")[-1]
+                    source = tid_to_offsets[sourceid]
+                    target = tid_to_offsets[targetid]
+                    gold_relations.add((did, source, target))
+    return gold_offsets, gold_relations
+
+def main():
+    pass
